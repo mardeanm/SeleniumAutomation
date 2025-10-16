794e057d3dc9c3ebce29185314f1d63482c0dc24
Mardean Montane <102998484+mardeanm@users.noreply.github.com>
2025-10-16 14:04:02 -0700

    Delete tests directory

--- tests/mocha_test.js
+++
@@ -1,8 +0,0 @@
-// const assert= require("assert")
-// const {describe} = require("mocha")
-
-// describe ('TestSuite', function(){
-//     it('Testcase1', function(){
-//         assert.equal("1", "1", "one is equal to one")
-//     })
-// })
